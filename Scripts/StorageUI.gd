6cf87b90dc9b9951c8644492952a0bb07b13fbf2
cursed144 <107793642+cursed144@users.noreply.github.com>
2025-03-21 00:14:34 +0200

    Storage system

---
+++ Scripts/StorageUI.gd
@@ -0,0 +1,64 @@
+extends Node2D
+
+var selected_max_count = 0
+var current_count = 1
+var selected_id = -1
+var boxes: Node2D
+
+var new_box = preload("res://Scenes/box.tscn")
+
+
+func select(id, count, texture):
+	$BottomButtons.show()
+	$BottomButtons/Icon.set_texture(texture)
+	selected_max_count = count
+	current_count = 1
+	selected_id = id
+	handle_enabling()
+
+
+func handle_enabling():
+	if current_count == selected_max_count:
+		$BottomButtons/Add.disabled = true
+	else:
+		$BottomButtons/Add.disabled = false
+	
+	if current_count == 1:
+		$BottomButtons/Sub.disabled = true
+	else:
+		$BottomButtons/Sub.disabled = false
+	
+	$BottomButtons/Count.text = str(current_count)
+
+
+func update_boxes(box: Node2D) -> void:
+	boxes = box
+
+
+func _on_sub_pressed() -> void:
+	current_count -= 1
+	handle_enabling()
+
+func _on_add_pressed() -> void:
+	current_count += 1
+	handle_enabling()
+
+
+func _on_get_pressed() -> void:
+	var box = boxes.get_child(selected_id)
+	
+	if selected_max_count == current_count:
+		box.reparent(get_node("../../Box"))
+		box.position = Vector2(0, -100)
+		box.show()
+		get_node("../../").close_storage_ui()
+		return
+	
+	var node = new_box.instantiate()
+	node.init(box.item_id, current_count, box.uname, box.get_node("ItemSprite").texture)
+	get_node("../../Box").add_child(node)
+	node.position = Vector2(0, -100)
+	node.show()
+	
+	box.count -= current_count
+	get_node("../../").close_storage_ui()
--- Scripts/box.gd
+++ Scripts/box.gd
@@ -1,1 +1,12 @@
 extends StaticBody2D
+
+@export var item_id: int
+@export var count: int
+@export var uname: String
+
+
+func init(id: int, num: int, title: String, texture: Texture2D) -> void:
+	item_id = id
+	count = num
+	uname = title
+	#$ItemSprite.set_texture(texture)
