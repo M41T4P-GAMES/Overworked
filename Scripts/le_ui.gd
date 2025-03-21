e67de62d32cbb6a5cb1fc82456b581bb40fdb766
toniabv69 <toni.s.angelov.2021@elsys-bg.org>
2025-03-21 18:05:41 +0200

    some hud stuff

---
+++ Scripts/le_ui.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _physics_process(delta: float) -> void:
+	$TextureRect/Label.text = "$" + str(GameStats.money)
+	update_timer(9)
+
+
+func update_timer(time_remaining: int):
+	var minutes = int(floor(time_remaining / 60))
+	var seconds = int(time_remaining % 60)
+	$TextureRect/Label2.text = "0" + str(minutes) + ":" + ("0" if seconds < 10 else "") + str(seconds)
