4c8f93c5b59d86694b46b1c7c9060af7924da539
KNProNoob <knpronoob@gmail.com>
2025-03-21 11:03:28 +0200

    Added the player sprite and animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,7 +4,7 @@ const SPEED = 65
 const MAX_SPEED = 400
 var selected_area = null
 var item_tab = preload("res://Scenes/item_tab.tscn")
-
+var facing : Vector2i
 
 func _ready() -> void:
 	set_multiplayer_authority(int(name))
@@ -29,7 +29,75 @@ func _physics_process(_delta: float) -> void:
 		else:
 			linear_velocity.x = move_toward(linear_velocity.x, 0, SPEED)
 			linear_velocity.y = move_toward(linear_velocity.y, 0, SPEED)
-
+		 
+		
+		#Animations
+		if input != Vector2.ZERO:
+			if input.x < 0 and input.y < 0:
+				$Sprite2D.flip_h = true
+				$Sprite2D.play("walking_back_right")
+				
+			elif input.x > 0 and input.y < 0:
+				$Sprite2D.flip_h = false
+				$Sprite2D.play("walking_back_right")
+				facing = Vector2i(1, -1)
+			elif input.x < 0 and input.y > 0:
+				$Sprite2D.flip_h = true
+				$Sprite2D.play("walking_front_right")
+				facing = Vector2i(-1, 1)
+			elif input.x > 0 and input.y > 0:
+				$Sprite2D.flip_h = false
+				$Sprite2D.play("walking_front_right")
+				facing = Vector2i(1, 1)
+			elif input == Vector2.UP:
+				$Sprite2D.flip_h = false
+				$Sprite2D.play("walking_back")
+				facing = Vector2i.UP
+			elif input == Vector2.DOWN:
+				$Sprite2D.flip_h = false
+				$Sprite2D.play("walking_front")
+				facing = Vector2i.DOWN
+			elif input == Vector2.LEFT:
+				$Sprite2D.flip_h = true
+				$Sprite2D.play("walking_right")
+				facing = Vector2i.LEFT
+			elif input == Vector2.RIGHT:
+				$Sprite2D.flip_h = false
+				$Sprite2D.play("walking_right")
+				facing = Vector2i.RIGHT
+		else:
+			if facing.x < 0 and facing.y < 0:
+				$Sprite2D.flip_h = true
+				$Sprite2D.play("idle_back_right")
+				
+			elif facing.x > 0 and facing.y < 0:
+				$Sprite2D.flip_h = false
+				$Sprite2D.play("idle_back_right")
+				
+			elif facing.x < 0 and facing.y > 0:
+				$Sprite2D.flip_h = true
+				$Sprite2D.play("idle_front_right")
+				
+			elif facing.x > 0 and facing.y > 0:
+				$Sprite2D.flip_h = false
+				$Sprite2D.play("idle_front_right")
+				
+			elif facing == Vector2i.UP:
+				$Sprite2D.flip_h = false
+				$Sprite2D.play("idle_back")
+				
+			elif facing == Vector2i.DOWN:
+				$Sprite2D.flip_h = false
+				$Sprite2D.play("idle_front")
+				
+			elif facing == Vector2i.LEFT:
+				$Sprite2D.flip_h = true
+				$Sprite2D.play("idle_right")
+				
+			elif facing == Vector2i.RIGHT:
+				$Sprite2D.flip_h = false
+				$Sprite2D.play("idle_right")
+				
 
 func open_storage_ui(boxes: Node2D) -> void:
 	freeze = true
