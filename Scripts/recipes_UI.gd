7a87e8741911e7f6b87877c0961a19ce61221a34
NoNeedForQuestions <105724486+AlexanderFrom100@users.noreply.github.com>
2025-03-21 17:03:46 +0200

    Computer UI ver.1
    Incomming orders mainly

--- Scripts/recipes_UI.gd
+++ Scripts/recipes_UI.gd
@@ -1,8 +1,11 @@
 extends Control
 
-var recipes = [];
-func _ready() -> void:
-	parse_recipes()
-	
-func parse_recipes():
-	var file = FileAccess.open("res://recipe)
+#var recipes = []
+
+#func _ready() -> void:
+#	parse_json()
+
+#func parse_json():
+	#var file = FileAccess.open("res://recipes.json", FileAccess.READ)
+	#var text = file.get_as_text()
+	#recipes = JSON.parse_string(text)
